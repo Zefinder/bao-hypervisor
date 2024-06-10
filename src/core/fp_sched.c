89b2e050938e3fb0ded77fac2b5e857b221a230a
Zefinder <jakubiak@insa-toulouse.fr>
2024-06-10 16:31:03 +0200

    Began memory delay in hypervisor

--- src/core/fp_sched.c
+++ src/core/fp_sched.c
@@ -5,10 +5,14 @@
 #include <spinlock.h>
 #include <interrupts.h>
 #include <ipi.h>
+#include <generic_timer.h>
 
 static spinlock_t memory_lock = SPINLOCK_INITVAL;
 static volatile int64_t memory_requests[NUM_CPUS] = {[0 ... NUM_CPUS - 1] = TOKEN_NULL_PRIORITY};
 static volatile struct memory_token memory_token = {TOKEN_NULL_OWNER, TOKEN_NULL_PRIORITY};
+// TODO Add the ifdef thing
+static volatile uint64_t next_fetch_time_array[NUM_CPUS] = {[0 ... NUM_CPUS - 1] = 0};
+static volatile uint64_t current_memory_fetch_time[NUM_CPUS] = {[0 ... NUM_CPUS - 1] = 0};
 
 uint64_t fp_request_access(uint64_t dec_prio)
 {
@@ -20,27 +24,53 @@ uint64_t fp_request_access(uint64_t dec_prio)
 
     spin_lock(&memory_lock);
 
+    // TODO ifdef
+    // If current time < next ok time then indicate how much time left to wait
+    // Else it means that it's ok to check the priority
+    uint64_t current_time = generic_timer_read_counter();
+    uint64_t next_fetch_time = next_fetch_time_array[cpu_id];
+    if (cpu_id != 0)
+    {
+        INFO("current_time=%ld, next_fetch_time=%ld (CPU %d)", current_time, next_fetch_time, cpu_id);
+    }
+    if (current_time < next_fetch_time)
+    {
+        union memory_request_answer answer = {{.ack = FP_REQ_RESP_NACK, .ttw = next_fetch_time - current_time}};
+        spin_unlock(&memory_lock);
+        return answer.raw;
+    }
+
     // Setting request priority in the array of requests
     memory_requests[cpu_id] = priority;
 
     // If it has greater priority, then...
     if (priority > memory_token.priority)
     {
-        // If someone had access to the memory, we send an IPI to pause
+        // If someone (other than the current owner) had access to the memory, we send an IPI to pause
         if (memory_token.owner != TOKEN_NULL_OWNER && (uint64_t)memory_token.owner != cpu_id)
         {
-            // INFO("CPU %d takes the memory token from CPU %d!", cpu_id, memory_token.owner);
             ipi_data_t ipi_data = {{0, IPI_IRQ_PAUSE}};
             send_ipi((cpuid_t)memory_token.owner, FPSCHED_EVENT, ipi_data);
+
+            // TODO ifdef
+            // Freeze the "fetch timer" for the one that was fetching
+            // That means add the current time to the current memory time (current time - start time)
+            current_memory_fetch_time[memory_token.owner] += current_time - next_fetch_time_array[memory_token.owner];
         }
 
         // Update the access token data
         memory_token.owner = (int64_t)cpu_id;
         memory_token.priority = priority;
+
+        // TODO ifdef
+        // Put in the next_fetch_array the start time (the value is not used when fetching)
+        next_fetch_time_array[cpu_id] = current_time;
     }
 
     // Returning FP_REQ_RESP_ACK if memory access granted
     int got_token = (memory_token.owner == (int64_t)cpu_id);
+    if (cpu_id != 0)
+        INFO("END OF MEMORY REQUEST");
     spin_unlock(&memory_lock);
 
     return got_token ? FP_REQ_RESP_ACK : FP_REQ_RESP_NACK;
@@ -52,7 +82,6 @@ void fp_revoke_access()
     int cpu_id = cpu()->id;
 
     spin_lock(&memory_lock);
-    // INFO("Revoke access for CPU %d", cpu_id);
 
     // Remove request (even if not using it, e.g. timed out)
     memory_requests[cpu_id] = TOKEN_NULL_PRIORITY;
@@ -60,6 +89,19 @@ void fp_revoke_access()
     // If owner of the access token then...
     if (memory_token.owner == (int64_t)cpu_id)
     {
+        // TODO ifdef
+        // Add the time to the current fetching time and compute next possible fetch (3 * time needed)
+        uint64_t current_time = generic_timer_read_counter();
+        uint64_t time_taken = current_memory_fetch_time[cpu_id] + (current_time - next_fetch_time_array[cpu_id]);
+        next_fetch_time_array[cpu_id] = current_time + 3 * time_taken;
+        if (cpu_id != 0)
+        {
+            INFO("current_time=%ld, time_taken=%ld, current_time+3*time_taken=%ld (CPU %d)", current_time, time_taken, current_time + 3 * time_taken, cpu_id);
+        }
+        
+        // Reset time taken
+        current_memory_fetch_time[cpu_id] = 0;
+
         // Reset token
         memory_token.owner = TOKEN_NULL_OWNER;
         memory_token.priority = TOKEN_NULL_PRIORITY;
@@ -77,10 +119,12 @@ void fp_revoke_access()
         // If found, send an IPI to resume the task
         if (memory_token.owner != TOKEN_NULL_OWNER)
         {
-            // INFO("Giving access to person waiting: CPU %d", memory_token.owner);
             ipi_data_t ipi_data = {{0, IPI_IRQ_RESUME}};
             send_ipi((cpuid_t)memory_token.owner, FPSCHED_EVENT, ipi_data);
-            // INFO("Access given to CPU %d", memory_token.owner);
+
+            // TODO ifdef
+            // Unfreeze the "fetch timer" for the new fetcher. That means reset the start time
+            current_memory_fetch_time[memory_token.owner] = current_time;
         }
     }
 
--- src/core/inc/fp_sched.h
+++ src/core/inc/fp_sched.h
@@ -18,6 +18,18 @@ struct memory_token
     int64_t priority;
 };
 
+/* Answer of hypervisor after a memory request */
+union memory_request_answer
+{
+    struct
+    {
+        uint64_t ack:1;     // 0 = no, 1 = yes
+        uint64_t ttw:63;    // Time to wait in case of no (0 = no waiting, just not prio)
+    };
+    uint64_t raw;
+};
+
+
 /* Memory request answers */
 #define FP_REQ_RESP_ACK 1
 #define FP_REQ_RESP_NACK 0
