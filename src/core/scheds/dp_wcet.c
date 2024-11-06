a27a7b58598fbb6fda2600ddc835a4e1dc18e9eb
Zefinder <jakubiak@insa-toulouse.fr>
2024-11-06 09:59:19 +0100

    Changed name for DP-WCET (TODO verify)

--- src/core/scheds/dp_wcet.c
+++ src/core/scheds/dp_wcet.c
@@ -12,8 +12,9 @@ static spinlock_t memory_lock = SPINLOCK_INITVAL;
 static volatile uint64_t memory_requests[PLAT_CPU_NUM] = {[0 ... PLAT_CPU_NUM - 1] = TOKEN_NULL_PRIORITY};
 static volatile struct memory_token memory_token = {(uint64_t)TOKEN_NULL_OWNER, (uint64_t)TOKEN_NULL_PRIORITY};
 static volatile uint64_t execution_time[PLAT_CPU_NUM] = {[0 ... PLAT_CPU_NUM - 1] = 0};
-static volatile uint64_t next_fetch_time_array[PLAT_CPU_NUM] = {[0 ... PLAT_CPU_NUM - 1] = 0};
-static volatile uint64_t current_memory_fetch_time[PLAT_CPU_NUM] = {[0 ... PLAT_CPU_NUM - 1] = 0};
+static volatile uint64_t start_prefetch[PLAT_CPU_NUM] = {[0 ... PLAT_CPU_NUM - 1] = 0};
+static volatile uint64_t next_prefetch[PLAT_CPU_NUM] = {[0 ... PLAT_CPU_NUM - 1] = 0};
+static volatile uint64_t prefetch_time[PLAT_CPU_NUM] = {[0 ... PLAT_CPU_NUM - 1] = 0};
 
 // Highest priority => closer number to 0 (0 being the highest)
 uint64_t request_memory_access(uint64_t priority, uint64_t wcet)
@@ -26,26 +27,25 @@ uint64_t request_memory_access(uint64_t priority, uint64_t wcet)
     // If current time < next ok time then indicate how much time left to wait
     // Else it means that it's ok to check the priority
     uint64_t current_time = generic_timer_read_counter();
-    uint64_t next_fetch_time = next_fetch_time_array[cpu_id];
     uint64_t low_prio_time = 0;
-    if (current_time < next_fetch_time)
+    uint64_t current_priority = priority;
+    if (current_time < next_prefetch[cpu_id])
     {
         // Set time being in low prio for the cpu
-        low_prio_time = next_fetch_time - current_time;
+        low_prio_time = next_prefetch[cpu_id] - current_time;
 
-        // Setting request priority in the array of requests
-        memory_requests[cpu_id] = priority + PLAT_CPU_NUM;
+        // Adding the number of CPU to the priorty (lower it)
+        current_priority += PLAT_CPU_NUM;
     }
-    else
-    {
-        memory_requests[cpu_id] = priority;
-    }
+
+    // Setting request priority in the array of requests
+    memory_requests[cpu_id] = current_priority;
 
     // Save the execution time
     execution_time[cpu_id] = wcet;
 
     // If it has lower priority, then...
-    if (priority < memory_token.priority)
+    if (current_priority < memory_token.priority)
     {
 
         // If someone (other than the current owner) had access to the memory, we send an IPI to pause
@@ -58,16 +58,16 @@ uint64_t request_memory_access(uint64_t priority, uint64_t wcet)
             // Freeze the "fetch timer" for the one that was fetching
             // That means add the current time to the current memory time (current time - start time)
             current_time = generic_timer_read_counter();
-            current_memory_fetch_time[memory_token.owner] += current_time - next_fetch_time_array[memory_token.owner];
+            prefetch_time[memory_token.owner] += current_time - start_prefetch[memory_token.owner];
         }
 
         // Put in the next_fetch_array the start time (the value is not used when fetching)
         current_time = generic_timer_read_counter();
-        next_fetch_time_array[cpu_id] = current_time;
+        start_prefetch[cpu_id] = current_time;
 
         // Update the access token data
         memory_token.owner = cpu_id;
-        memory_token.priority = priority;
+        memory_token.priority = current_priority;
     }
 
     // Returning the answer and the time in low prio
@@ -93,12 +93,11 @@ void revoke_memory_access()
     {
         // Add the time to the current fetching time and compute next possible fetch (3 * time needed)
         uint64_t current_time = generic_timer_read_counter();
-        uint64_t time_taken = current_memory_fetch_time[cpu_id] + (current_time - next_fetch_time_array[cpu_id]);
-        INFO("%lld", execution_time[cpu_id]);
-        next_fetch_time_array[cpu_id] = current_time + (execution_time[cpu_id] - time_taken);
+        uint64_t time_taken = prefetch_time[cpu_id] + (current_time - start_prefetch[cpu_id]);
+        next_prefetch[cpu_id] = current_time + (execution_time[cpu_id] - time_taken);
 
         // Reset time taken
-        current_memory_fetch_time[cpu_id] = 0;
+        prefetch_time[cpu_id] = 0;
 
         // Reset token
         memory_token.owner = (uint64_t)TOKEN_NULL_OWNER;
@@ -122,7 +121,7 @@ void revoke_memory_access()
 
             // Unfreeze the "fetch timer" for the new fetcher. That means reset the start time
             current_time = generic_timer_read_counter();
-            next_fetch_time_array[memory_token.owner] = current_time;
+            strcat[memory_token.owner] = current_time;
         }
     }
 
